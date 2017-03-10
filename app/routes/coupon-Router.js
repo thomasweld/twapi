ab61f62fc73f1ad743845977a8f99aa4ae580a94
wgimson <wgimson@gmail.com>
2017-03-09 22:18:17 -0500

    Business logic for events/coupons removed from routers

--- app/routes/coupon-Router.js
+++ app/routes/coupon-Router.js
@@ -1,14 +1,9 @@
-var express       = require('express'),
-    dateFunctions = require('../utils/date_functions.js'),
-    mongoose      = require('mongoose'),
-    idObj         = require('../utils/idGen'),
-    Coupon        = require('../../app/models/coupon'),
-    winston       = require('../../winston.config');
+var express          = require('express'),
+
+    CouponController = require('../controllers/CouponController'),
+    winston          = require('../../winston.config');
 
 var router = express.Router();
-
-// TODO - this is just for testing, remove later
-var ids = idObj;
 
 router.use(function(req, res, next) {
     winston.log('info', '---A Coupon route is being called---');
@@ -17,77 +12,21 @@ router.use(function(req, res, next) {
 
 router.route('/')
     .post(function(req, res) {
-        var coupon = new Coupon();
-        coupon.orderId    = idObj.orderId;
-        coupon.eventId    = idObj.eventId;
-        coupon.amount     = req.body.amount;
-        winston.log('info', coupon.amount);
-        coupon.syncedAt   = req.body.syncedAt;
-        coupon.redeemedAt = req.body.redeemedAt;
-        coupon.createdAt  = dateFunctions.getDate();
-        coupon.updatedAt  = req.body.updatedAt;
-        coupon.code = req.body.code;
-
-        coupon.save(function(err) {
-            if (err) {
-                winston.log('error', '---POST COUPON ERROR', { error: err });
-                res.send(err);
-            }
-            res.json(coupon);
-        });
+        return CouponController.createCoupon(req, res);
     })
     .get(function(req, res) {
-        winston.log('info', '--GET All Coupons--');
-        Coupon.find(function(err, coupons) {
-            if (err) {
-                winston.log('error', '---GET ALL COUPONS ERROR', { error: err });
-                res.send(err);
-            }
-            res.json(coupons);
-        });
+       return CouponController.getAllCoupons(req, res);
     });
     
 router.route('/:couponId')
     .get(function(req, res) {
-        winston.log('info', '---GET Coupon by ID---');
-        Coupon.findById(req.params.couponId, function(err, coupon) {
-            if (err) {
-                winston.log('error', '---GET COUPON ERROR', { error: err });
-                res.send(err);
-            }
-            res.json(coupon);
-        });
+        return CouponController.getCouponById(req, res);
     })
     .put(function(req, res) {
-        winston.log('info', '---PUT Coupon by ID---');
-        Coupon.findById(req.params.couponId, function(err, updatedCoupon) {
-            if (err) {
-                res.send(err);
-            }
-
-            updatedCoupon.amount     = req.body.amount;
-            updatedCoupon.syncedAt   = req.body.syncedAt;
-            updatedCoupon.redeemedAt = req.body.redeemedAt;
-            updatedCoupon.updatedAt  = dateFunctions.getDate();
-
-            updatedCoupon.save(function(err, updateCoupon) {
-                if (err) { 
-                    winston.log('error', '---UPDATE COUPON ERROR', { error: err });
-                    res.send(err);
-                }
-                res.json(updatedCoupon);
-            });
-        });
+        return CouponController.updateCoupon(req, res);
     })
     .delete(function(req, res) {
-        winston.log('info', '---DELETE Coupon by ID---');
-        Coupon.remove({ _id: req.params.couponId }, function(err, coupon) {
-            if (err) {
-                winston.log('error', '---DELETE COUPON ERROR', { error: err });
-                res.send(err);
-            }
-            res.json(coupon);
-        });
+         return CouponController.deleteCoupon(req, res);
     });
 
 module.exports = router;
--- app/routes/event-Router.js
+++ app/routes/event-Router.js
@@ -1,8 +1,7 @@
-var express       = require('express'),
-    dateFunctions = require('../utils/date_functions.js'),
-    mongoose      = require('mongoose'),
-    Event         = require('../../app/models/event'),
-    winston       = require('../../winston.config'); 
+var express         = require('express'),
+
+    EventController = require('../controllers/EventController'),
+    winston         = require('../../winston.config'); 
 
 var router = express.Router();
 
@@ -13,100 +12,26 @@ router.use(function(req, res, next) {
  
 router.route('/')
     .post(function(req, res) {
-         console.log('POST Event');
-        var event = new Event();
-        event.title            = req.body.title;
-        event.description      = req.body.description;
-        event.startDate        = req.body.startDate;
-        event.endDate          = req.body.endDate;
-        event.userId           = mongoose.Types.ObjectId();
-        event.createdAt        = dateFunctions.getDate();
-        event.updatedAt        = null;
-        event.status           = req.body.status;
-        event.imageFileName    = req.body.imageFileName;
-        event.imageContentType = req.body.imageContentType;
-        event.imageFileSize    = req.body.imageFileSize;
-        event.imageUpdatedAt   = dateFunctions.getDate();
-        event.location         = req.body.location;
-        event.organizationId   = req.body.organizationId;
-        event.isFeatured       = req.body.isFeatured;
-
-        event.save(function(err) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(event);
-        });
+         return EventController.createEvent(req, res);
     })
     .get(function(req, res) {
-         winston.log('info', '---GET Events---');
-        Event.find(function(err, events) {
-            if (err) {
-                winston.log('error', '---GET EVENTS ERROR---', { error: err });
-                res.send(err);
-            }
-            res.json(events);
-        });
+       return EventController.getAllEvents(req, res);
     });
 
 router.route('/featured')
     .get(function(req, res) {
-        winston.log('info', '---GET Featured Events---');
-        Event.find({ 'isFeatured': 'true'}, function(err, events) {
-            if (err) {
-                winston.log('error', '---GET FEATURED EVENTS ERROR---', { error: err });
-                res.send(err);
-            }
-            res.json(events);
-        });
+       return EventController.getAllFeaturedEvents(req, res);
     });
     
 router.route('/:eventId')
     .get(function(req, res) {
-        winston.log('info', '---GET Event by ID---');
-        Event.findById(req.params.eventId, function(err, event) {
-            if (err) {
-                winston.log('error', '---GET EVENT BY ID ERROR---', { error: err });
-                res.send(err);
-            }
-            res.json(event);
-        });
+        return EventController.getEventById(req, res);
     })
     .put(function(req, res) {
-         winston.log('info', '---PUT Event by ID---');
-        Event.findById(req.params.eventId, function(err, updatedEvent) {
-            if (err) {
-                winston.log('error', '---GET EVENT BY ID ERROR---', { error: err });
-                res.send(err);
-            }
-
-            updatedEvent.title = req.body.title;
-            updatedEvent.description = req.body.description;
-            updatedEvent.startDate = req.body.startDate;
-            updatedEvent.endDate = req.body.endDate;
-            updatedEvent.createdAt = dateFunctions.getDate();
-            updatedEvent.status = req.body.status;
-            updatedEvent.imageFileName = req.body.imageFileName;
-            updatedEvent.location = req.body.location;
-
-            updatedEvent.save(function(err, updatedEvent) {
-                if (err) { 
-                    winston.log('error', '---UPDATE EVENT ERROR---', { error: err });
-                    res.send(err);
-                }
-                res.json(updatedEvent);
-            });
-        });
+       return EventController.updateEvent(req, res); 
     })
     .delete(function(req, res) {
-         winston.log('info', '---DELETE Event by ID---');
-        Event.remove({ _id: req.params.eventId }, function(err, event) {
-            if (err) {
-                winston.log('error', '---DELETE EVENT ERROR---', { error: err });
-                res.send(err);
-            }
-            res.json(event);
-        });
+       return EventController.deleteEvent(req, res);
     });
 
 module.exports = router;
